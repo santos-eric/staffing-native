b3d3db7ce3f68514a84c577ed9a808c39b1fe892
santos-eric <esantosdev77@gmail.com>
2023-04-20 06:42:57 -0600

    structured welcome and popularjobs component, tied to styles

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -40,6 +40,9 @@ const Welcome = () => {
             <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
           </TouchableOpacity>
           )}
+          keyExtractor={item => item}
+          contentContainerStyle={{ columnGap: SIZES.small }}
+          horizontal
         />
       </View>
     </View>
