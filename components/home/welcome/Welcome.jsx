9b2d455ccf29edcd717544a643c5d3d04a1ed0f8
santos-eric <esantosdev77@gmail.com>
2023-04-20 06:19:27 -0600

    first commit, built started react native with expo app, some styles and empty components are prebuilt, no connected to an API

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,49 @@
+import {useState} from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+
+import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
+
+const jobTypes = ["General-Labor","Catering","Cooking"]
+
+const Welcome = () => {
+  const router = useRouter()
+  const [activeJobType, setActiveJobTye] =useState('General-labor')
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Eric</Text>
+        <Text style={styles.welcomeMessage}>Find Daily Work</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={ styles.searchInput} value="" onChange={() => {}} placeholder='What type of work are you looking for?' />
+        </View>
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.tab(activeJobType, item)} onPress={() => {
+              setActiveJobTye(item)
+              router.push(`/search/${item}`)
+          }}>
+            <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+          </TouchableOpacity>
+          )}
+        />
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
